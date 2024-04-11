d7b741b91eab8a958be065f39cb79a3d62f10097
papula-a <pa20381@auis.edu.krd>
2024-04-11 16:26:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+// index.js
 
-const titleCased = () => {
-  return tutorials
+const titleCase = (str) => {
+  return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+};
+
+function titleCased() {
+  const tutorials = [
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
+  
+  return tutorials.map(titleCase);
 }
+
+module.exports = {
+  titleCased
+};
